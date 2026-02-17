5bf5bdadea60f5f5bebdda3466d9e00dfc293856
Oliver Brito <oliver.brito@audienceview.com>
2026-02-17 11:23:49 -0300

    Load environment variables for default credentials in authHeaders

--- endpoints/utils/authHeaders.js
+++ endpoints/utils/authHeaders.js
@@ -1,11 +1,16 @@
 import { CURRENT_SESSION, CURRENT_COOKIES } from "../utils/sessionStore.js";
 import { filterCookieHeader } from "./cookieUtils.js";
+import dotenv from "dotenv";
+
+// Load .env for default credentials
+dotenv.config();
 
 export function authHeaders(extra = {}) {
   const base = {
     Accept: "application/json, text/plain, */*",
     "Content-Type": "application/json",
   };
+
   if (CURRENT_SESSION) base.Session = CURRENT_SESSION;
   if (CURRENT_COOKIES) base.Cookie = filterCookieHeader(CURRENT_COOKIES);
   return { ...base, ...extra };
